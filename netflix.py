c1d4b562e2d4fd3a69c51a8c7ee6e0e506954f24
MCEVERGARDEN <wahil.boulahbach.blog@gmail.com>
2022-11-04 21:55:44 +0100

    Netflix JSON datas fixed | Episodes and seasons fixed |

--- netflix.py
+++ netflix.py
@@ -1,12 +1,3 @@
-file = "./netflix/ViewingActivity.csv"
-import json
-with open(file,'r',encoding="utf-8")  as f:
-        f = f.read().splitlines()
-
-for x in range(len(f)):
-    f[x] = f[x].split(",")
-
-dico = {}
 
 class Manager:
     def __init__(self,profile,started,duration,title,bookmark,latest_bookmark):
@@ -15,28 +6,37 @@ class Manager:
         self.duration = duration
         self.bookmark = bookmark
         self.latest_bookmark = latest_bookmark
-        self.type = "type"
         self.title = title
         self.content = None
+
+
         toR = ["Saison","Partie"]
         if "pisode" in self.title:
+            to_cut = 0
             ver = False
             for x in toR:
                 if x in self.title:
                     to_cut = self.title.index(x)
+                    type_sea = self.title[to_cut:to_cut + len(x) +2]
                     ver = True
-            if ver == True:
-                crop =  self.title[:to_cut]
-                self.type = "Shows"
-                self.content = Show(profile,started,duration,title,bookmark,latest_bookmark).__dict__
-                if crop not in dico:
-                    dico[crop] = {}
+
+            if ver == False:
+                to_cut = self.title.index(f"\u00c9pisode")
+
+            crop =  self.title[:to_cut]
+            self.content = Show(profile,started,duration,title,bookmark,latest_bookmark).__dict__
+
+            if crop not in dico:
+                dico[crop] = {}
+            if ver == False:
                 dico[crop].update({self.content["ep"]:self.content})
             else:
-                self.content = item(profile,started,duration,title,bookmark,latest_bookmark).__dict__
-                dico[self.title]  = self.content
+                if type_sea not in dico[crop]:
+                    dico[crop][type_sea]={}
+                dico[crop][type_sea].update({self.content["ep"]:self.content})
+
+         
         else:
-            self.type = "Movies"
             self.content = item(profile,started,duration,title,bookmark,latest_bookmark).__dict__
             dico[self.title]  = self.content
         
@@ -48,13 +48,26 @@ class item:
         self.duration = duration
         self.bookmark = bookmark
         self.latest_bookmark = latest_bookmark
-        self.type = "type"
+        self.type = "Movie"
         self.title = title
 
 class Show(item):
     def __init__(self, profile, started, duration, title, bookmark, latest_bookmark):
         super().__init__(profile, started, duration, title, bookmark, latest_bookmark)
-        self.ep = self.title[self.title.index("Épisode"):self.title.index(")")]
+        index_ep = self.title.index("pisode")
+        self.ep = self.title[index_ep-1:index_ep + len("episode")+1]
+
+
+file = "./netflix/ViewingActivity.csv"
+import json
+with open(file,'r',encoding="utf-8")  as f:
+        f = f.read().splitlines()
+
+for x in range(len(f)):
+    f[x] = f[x].split(",")
+
+dico = {}
+
 
 f.sort()
 for toRemove in f:
